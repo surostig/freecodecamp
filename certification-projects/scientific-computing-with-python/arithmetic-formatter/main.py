cbf7c324df29aaa0ea48e4cf0ac5abe454f1ce61
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-06-30 13:49:20 -0500

    Update main.py

--- certification-projects/scientific-computing-with-python/arithmetic-formatter/main.py
+++ certification-projects/scientific-computing-with-python/arithmetic-formatter/main.py
@@ -1,26 +1,32 @@
+# list problems to be formatted
 problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+operators_allowed = ['+', '-']
+operators_disallowed = ['*', '/']
 
+# create arranger function for the problems
 def arithmetic_arranger(problems):
 
+# establish limits and operators allowed/disallowed
     limit = 5
-    operators_allowed = ['+', '-']
-    operators_disallowed = ['*', '/']
     
+# check whether operator is allowed or disallowed
     def operator_check(operator):
         if operator in operators_allowed:
             pass
         elif operator in operators_disallowed:
             raise ValueError("Error: Error: Operator must be '+' or '-'.")
-    
+
+# check if problem list is within limit    
     if len(problems) > limit:
         raise ValueError('Error: Too many problems.')
 
-    for problem in problems:
-        pass
+# proceed to arrange problems if list within limit
+
 
 def main():
     return arithmetic_arranger(problems)
 
+# run the arranger
 main()
 
 # select a test problem to analyze
@@ -33,4 +39,7 @@ for string in test:
     tests.append(type(string))
     tests.append(len(string))
     tests.append(string)
-    print(tests)
+    if string in operators_allowed + operators_disallowed:
+       print(f'The operator is {string}') 
+
+
