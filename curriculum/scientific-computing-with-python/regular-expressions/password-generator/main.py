632834c940c65aaa0d5b5f2c01f57cd6fe363b8c
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-07-01 21:57:12 -0500

    Create main.py

---
+++ curriculum/scientific-computing-with-python/regular-expressions/password-generator/main.py
@@ -0,0 +1,31 @@
+import re
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, '')
+        ]        
+
+    return password
+    
+# new_password = generate_password(8)
+# print(new_password)
+pattern = re.compile('l+')
+quote = 'Not all those who wander are lost.'
+print(pattern.search(quote))
