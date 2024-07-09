add6fa55fc7cbea03260a330d1c17289eefb075e
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-07-08 21:50:24 -0500

    Update main.py

--- curriculum/scientific-computing-with-python/algorithm-design/shortest-path-algorithm/main.py
+++ curriculum/scientific-computing-with-python/algorithm-design/shortest-path-algorithm/main.py
@@ -4,12 +4,14 @@ my_graph = {
     'C': [('B', 4), ('D', 7)],
     'D': [('A', 1), ('C', 7)]
 }
+
 def shortest_path(graph, start):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
     
-    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
-    
-shortest_path(my_graph, 'A')
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            pass
