2617997e1394c81896264de5248c765c799af028
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-07-12 22:29:15 -0500

    Update main.py

--- curriculum/scientific-computing-with-python/algorithm-design/shortest-path-algorithm/main.py
+++ curriculum/scientific-computing-with-python/algorithm-design/shortest-path-algorithm/main.py
@@ -1,3 +1,4 @@
+
 my_graph = {
     'A': [('B', 3), ('D', 1)],
     'B': [('A', 3), ('C', 4)],
@@ -5,21 +6,24 @@ my_graph = {
     'D': [('A', 1), ('C', 7)]
 }
 
-def shortest_path(graph, start):
+def shortest_path(graph, start, target = ''):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
+    
     while unvisited:
         current = min(unvisited, key=distances.get)
         for node, distance in graph[current]:
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
-                if paths[node][-1] == node:
-                    paths[node] = paths[current]
+                if paths[node] and paths[node][-1] == node:
+                    paths[node] = paths[current][:]
                 else:
                     paths[node].extend(paths[current])
                 paths[node].append(node)
-    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
-    
-#shortest_path(my_graph, 'A')
+        unvisited.remove(current)
+    targets_to_print = [target] if target else graph
+
+
+shortest_path(my_graph, 'A')
