6e01e8a8d89bd5105f5596b73f84a37eaa909f59
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-07-24 12:37:41 -0500

    Update main.py
    Steps 13-25

--- curriculum/scientific-computing-with-python/recursion/tower-of-hanoi/main.py
+++ curriculum/scientific-computing-with-python/recursion/tower-of-hanoi/main.py
@@ -6,7 +6,22 @@ rods = {
     'C': []
 }
 
-def move():
+def move(n, source, auxiliary, target):
+    # display starting configuration
     print(rods)
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+            forward = False
+            if not rods[target]:
+                forward = True
+            elif rods[source] and rods[source][-1] < rods[target][-1]:
+                forward = True
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+        elif remainder == 0:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
 
-move()
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
