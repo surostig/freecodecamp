d5a4650b7df7f1ddced2c1ade2794e7fffd6bcb2
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-07-30 21:44:25 -0500

    45-51

--- curriculum/scientific-computing-with-python/recursion/tower-of-hanoi/main.py
+++ curriculum/scientific-computing-with-python/recursion/tower-of-hanoi/main.py
@@ -7,11 +7,17 @@ rods = {
 
 def move(n, source, auxiliary, target):
     if n > 0:
+        # move n - 1 disks from source to auxiliary, so they are out of the way
+        move(n - 1, source, target, auxiliary)
         
-        move(n - 1, source, auxiliary, target)
+        # move the nth disk from source to target
+        rods[target].append(rods[source].pop())
         
-        # display starting configuration
+        # display our progress
         print(rods, '\n')
+        
+        # move the n - 1 disks that we left on auxiliary onto target
+        move(n - 1,  auxiliary, source, target)
               
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
