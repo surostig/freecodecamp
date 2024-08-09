501ed9abede68feb45b9ac9abf370bd9817fbb4a
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-08-08 20:24:21 -0500

    28-33

--- curriculum/scientific-computing-with-python/data-structures/merge-sort-algorithm/main.py
+++ curriculum/scientific-computing-with-python/data-structures/merge-sort-algorithm/main.py
@@ -1,7 +1,7 @@
 def merge_sort(array):
     if len(array) <= 1:
         return
-   
+    
     middle_point = len(array) // 2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
@@ -26,9 +26,16 @@ def merge_sort(array):
         array[sorted_index] = left_part[left_array_index]
         left_array_index += 1
         sorted_index += 1
-   
+    
     while right_array_index < len(right_part):
         array[sorted_index] = right_part[right_array_index]
         right_array_index += 1
         sorted_index += 1
 
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: '+ str(numbers))
