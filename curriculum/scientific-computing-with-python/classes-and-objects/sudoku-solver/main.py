761beb04825372616331046bbcc0f69f6add376b
Sebastian Rostig <90658282+surostig@users.noreply.github.com>
2024-08-13 23:41:55 -0500

    13-34

--- curriculum/scientific-computing-with-python/classes-and-objects/sudoku-solver/main.py
+++ curriculum/scientific-computing-with-python/classes-and-objects/sudoku-solver/main.py
@@ -2,6 +2,21 @@ class Board:
     def __init__(self, board):
         self.board = board
 
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+    def valid_in_col(self, col, num):
+        return all(self.board[row][col] != num for row in range(9))
+
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
   [0, 0, 0, 0, 0, 3, 7, 6, 2],
@@ -15,5 +30,4 @@ puzzle = [
 ]
 
 gameboard = Board(puzzle)
-
-print(gameboard.board)
+print(gameboard.valid_in_col(0, 1))
